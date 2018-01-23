0b0894cd0b8ef961b7dacf3b8680565d92b249e9
ddset <againhay@gmail.com>
2018-01-23 22:21:39 +0900

    Add files via upload

---
+++ c/assets/js/d.js
@@ -0,0 +1,8 @@
+function noEvent(){
+   if(event.keyCode == 116) {
+      event.keyCode = 2;
+      return false;
+   }else if(event.ctrlKey&& (event.keyCode==78 || event.keyCode == 82)) {
+      return false;
+   }
+}
